59e19b84be001d6d938532c32a2a8a8b42293323
s.yakimov <0x2f8f@gmail.com>
2019-02-28 16:59:49 +0300

    array

---
+++ array.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main()  {
+	array1()
+	array2()
+	array3()
+}
+
+func array1()  {
+	var a [10]int;
+	fmt.Println(a);
+}
+
+func array2()  {
+	var a [10]int;
+	a[1] = 1
+	a[2] = 2
+	fmt.Println(a);
+}
+
+func array3()  {
+	a := [4]int{1,2,3}
+	fmt.Println(a);
+}
