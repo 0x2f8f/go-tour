4d2968a72fdab7f24d88664bbe78ab46da316915
s.yakimov <0x2f8f@gmail.com>
2019-02-28 17:28:58 +0300

    slices

---
+++ slices.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main()  {
+	slices1();
+	slices2();
+	slices3();
+	slices4();
+}
+
+func slices1()  {
+	var a [10]int; 	//массив
+	var s []int;	//слайс (он же срез)
+	fmt.Println(a, s);
+}
+
+//несколько способов объявления слайсов
+//смотрим типы
+func slices2()  {
+	a := [6]int{0,1,2,3,4,5}
+	var s []int = a[0:3];
+	s2 := a[3:6]
+
+	fmt.Println(a, s, s2);
+	fmt.Printf("%T %T %T\n", a, s, s2)
+}
+
+//слайсы - это указатели на массивы
+//при изменении элемента массива, меняется значение и у слайса
+func slices3()  {
+	a := [6]int{0,1,2,3,4,5}
+	var s1 []int = a[0:3]
+	var s2 []int = a[2:4]
+	fmt.Println(a, s1, s2);
+
+	a[2] = 22
+	fmt.Println(a, s1, s2);
+}
+
+//литералы срезов
+//в этих ситуациях на самом деле вначале в памяти создаётся массив,
+// а после слайс, который на негол ссылается
+func slices4()  {
+	si := []int{1, 2, 3}
+	sb := []bool{true, true, false}
+	ss := []string{"one", "two", "three"}
+	fmt.Println(si, sb, ss)
+
+	st := []struct {
+		i int
+		b bool
+	}{
+		{1, true},
+		{2, false},
+	}
+	fmt.Println(st)
+}
