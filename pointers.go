11130562e0b250e85ae8db2763ce854c50fdbd37
s.yakimov <0x2f8f@gmail.com>
2019-02-28 16:30:41 +0300

    pointers

--- pointers.go
+++ pointers.go
@@ -4,11 +4,18 @@ func main() {
 	pointers1()
 }
 
+//указатели
 func pointers1()  {
 	var p *int;
 	i := 42
 	p = &i
-	println(*p)
+	println(*p) //42
+
 	i++
-	println(*p)
+	println(*p) //43
+
+	*p = 40
+	println(i) //40
+	i++
+	println(*p) //41
 }
